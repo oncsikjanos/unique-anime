1d7af8a2c4ef6888a88917ef928b67e175caa75d
Oncsik János <janoka82@gmail.com>
2026-02-09 23:39:20 +0100

    add caching logic

---
+++ frontend/src/app/services/data.service.ts
@@ -0,0 +1,44 @@
+import {inject, Injectable} from '@angular/core';
+import {FirestoreService} from "./firestore.service";
+import {map, tap, of, Observable} from "rxjs";
+import {User} from "../models/User";
+import {Anime} from "../models/Anime";
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DataService {
+  private firestoreService: FirestoreService = inject(FirestoreService);
+
+  getUserData(): Observable<User[]> {
+    return this.getDataFromCache('userDataList', this.initUsersListCache);
+  }
+
+  getAnimeData(userName: string): Observable<Anime[]> {
+    return this.getDataFromCache(userName + '_animeDataList', () => this.initAnimeListCache(userName));
+  }
+
+  private getDataFromCache<T>(cacheKey: string, initCacheMethod: () => Observable<T>): Observable<T> {
+    const cachedData = localStorage.getItem(cacheKey);
+
+    if (cachedData) {
+      return of(JSON.parse(cachedData));
+    }
+
+    return initCacheMethod();
+  }
+
+  private initUsersListCache(): Observable<User[]> {
+    return this.firestoreService.getUserList().pipe(
+        map(users => [...users].sort((a, b) => b.unique - a.unique)),
+        tap(users => localStorage.setItem('userDataList', JSON.stringify(users)))
+    );
+  }
+
+  private initAnimeListCache(userName: string): Observable<Anime[]> {
+    return this.firestoreService.getAnimeList(userName).pipe(
+        map(animes => [...animes].sort((a, b) => b.point - a.point)),
+        tap(animes => localStorage.setItem(userName + '_animeDataList', JSON.stringify(animes)))
+    );
+  }
+}
