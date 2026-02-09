e19c4fddf3aee7410f3985e536cff1570c6039ea
Oncsik János <janoka82@gmail.com>
2026-02-09 23:54:19 +0100

    caches fix

--- frontend/src/app/services/firestore.service.ts
+++ frontend/src/app/services/firestore.service.ts
@@ -1,32 +1,26 @@
 import { Injectable, inject } from '@angular/core';
 import { collection, collectionData, Firestore } from '@angular/fire/firestore';
 import {Observable} from "rxjs";
-
-interface User {
-  name: string;
-  pfp: string;
-  completed: number;
-  unique: number;
-}
+import {User} from "../models/User";
+import {Anime} from "../models/Anime";
 
 @Injectable({
   providedIn: 'root'
 })
 export class FirestoreService {
-  firestore: Firestore = inject(Firestore);
+  private firestore: Firestore = inject(Firestore);
 
-  constructor() { }
-
-  getUserList(): Observable<any[]> {
-    const userCollection = collection(this.firestore, 'User')
-    return collectionData(userCollection) as Observable<any[]>;
+  getUserList(): Observable<User[]> {
+    return this.getDataFromFirestore<User>('User');
   }
 
-  getAnimeList(user: string): Observable<any[]> {
-    if (!user || user.trim() === '') {
-      return new Observable(subscriber => subscriber.next([])); // Return empty list if no user
-    }
-    const animeCollection = collection(this.firestore, "Anime/"+user+"/Uniques")
-    return collectionData(animeCollection)
+  getAnimeList(user: string): Observable<Anime[]> {
+    return this.getDataFromFirestore<Anime>("Anime/"+user+"/Uniques");
   }
+
+  private getDataFromFirestore<T>(collectionPath: string): Observable<T[]> {
+    const collectionRef = collection(this.firestore, collectionPath);
+    return collectionData(collectionRef) as Observable<T[]>;
+  }
+
 }
