07dceb01ab4bc4131f86fb025a67da51aaadbe54
squasheland <janoka82@gmail.com>
2026-05-04 20:21:10 +0200

    yml fix

---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBvfuY9YRVGYHrs0nOJpjfdyqU2MYwA7IQ",
+    authDomain: "toxictsuniqueanime.firebaseapp.com",
+    projectId: "toxictsuniqueanime",
+    storageBucket: "toxictsuniqueanime.firebasestorage.app",
+    messagingSenderId: "829336432036",
+    appId: "1:829336432036:web:6c383b47358b833a0aa531",
+    measurementId: "G-X484VGW1H9"
+}
